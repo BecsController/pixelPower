95898a0a9f2b96475975daa0448b6edf3fd100ea
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-06-03 16:20:49 +1200

    boards up

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 document.addEventListener('DOMContentLoaded', start)
 
 function start (){
+   createCanvas();
+   createPalette();
 
-createCanvas();
+  function createCanvas(){
+     let canvas = document.getElementById("myCanvas");
+     let sizey = 100;
+     let sizex = 50;
+     for (var i = 0; i < sizex; i++){
+       let row = document.createElement("tr");
+       for (var j = 0; j < sizey; j++){
+         let cell = document.createElement("td");
+         cell.width = "15px";
+         cell.height = "15px";
+         cell.classList.add("cell");
+         cell.setAttribute('id', '#row'+ i +'col' + j)
+         row.appendChild(cell);
+       }
+       canvas.appendChild(row);
+     }
+   }
+let cells = document.getElementsByClassName("cell");
 
-function createCanvas(){
-let size = 40;
-  for (var i = 0; i < size; i++){
-    $('table').append('<tr></tr>');
-    for (var j = 0; j < size; j++){
-      $('tr:last').append('<td></td>');
-      $('td').attr("class", 'cells');
-    }
-  }
+for (var k = 0; k < cells.length; k++){
+  cells[k].addEventListener("click", paint);
 }
 
-$('.cells').click(function(event){
-  var paint = $('#colorPicker').val();
-  $(event.target).css('background-color', paint);
-})
-let cells = document.getElementsByClassName("cells")
-cells.addEventListener("click", paint);
+function paint (cell){
+  event.target.classList.add("outline")
+}
 
+function createPalette(){
+  let palette = document.getElementById("palette");
+  let sizey = 5;
+  let sizex = 6;
+  for (var i = 0; i < sizex; i++){
+    let paletteRow = document.createElement("tr");
+    for (var j =0; j < sizey; j++){
+      let paletteCell = document.createElement("td");
+      paletteCell.width = '50px';
+      paletteCell.height = '50px';
+      paletteCell.setAttribute('id', 'p' + '#row'+ i +'col' + j)
+      paletteRow.appendChild(paletteCell);
+    }
+    palette.appendChild(paletteRow);
+  }
+}
 }
