0c8cd562e28e4dc1466d42b7ac7bcf260a56721b
Rebecca Dugdale <bec.dugdale16@gmail.com>
2018-06-03 17:21:08 +1200

    palette colors

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function paint (cell){
 function createPalette(){
   let palette = document.getElementById("palette");
   let sizey = 5;
-  let sizex = 6;
+  let sizex = 4;
   for (var i = 0; i < sizex; i++){
     let paletteRow = document.createElement("tr");
     for (var j =0; j < sizey; j++){
@@ -47,4 +47,45 @@ function createPalette(){
     palette.appendChild(paletteRow);
   }
 }
+var paletteOne = document.getElementById("p#row0col0");
+var paletteTwo = document.getElementById("p#row0col1");
+var paletteThree = document.getElementById("p#row0col2");
+var paletteFour = document.getElementById("p#row0col3");
+var paletteFive = document.getElementById("p#row0col4");
+var paletteSix = document.getElementById("p#row1col0");
+var paletteSeven = document.getElementById("p#row1col1");
+var paletteEight = document.getElementById("p#row1col2");
+var paletteNine = document.getElementById("p#row1col3");
+var paletteTen = document.getElementById("p#row1col4");
+var paletteEleven = document.getElementById("p#row2col0");
+var paletteTwelve = document.getElementById("p#row2col1");
+var paletteThirteen = document.getElementById("p#row2col2");
+var paletteFourteen = document.getElementById("p#row2col3");
+var paletteFifteen = document.getElementById("p#row2col4");
+var paletteSixteen = document.getElementById("p#row3col0");
+var paletteSeventeen = document.getElementById("p#row3col1");
+var paletteEighteen = document.getElementById("p#row3col2");
+var paletteNineteen = document.getElementById("p#row3col3");
+var paletteTwenty = document.getElementById("p#row3col4");
+
+paletteOne.classList.add("red");
+paletteTwo.classList.add("redder");
+paletteThree.classList.add("orange");
+paletteFour.classList.add("lightOrange");
+paletteFive.classList.add("yellow");
+paletteSix.classList.add("lightGreen");
+paletteSeven.classList.add("midGreen");
+paletteEight.classList.add("darkGreen");
+paletteNine.classList.add("turquoise");
+paletteTen.classList.add("aqua");
+paletteEleven.classList.add("lightBlue");
+paletteTwelve.classList.add("midBlue");
+paletteThirteen.classList.add("darkBlue");
+paletteFourteen.classList.add("indigo");
+paletteFifteen.classList.add("grape");
+paletteSixteen.classList.add("lightPink");
+paletteSeventeen.classList.add("midPink");
+paletteEighteen.classList.add("darkPink");
+paletteNineteen.classList.add("white");
+paletteTwenty.classList.add("outline");
 }
